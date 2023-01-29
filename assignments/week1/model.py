4c807f1f7df86da78b3733b6a8aee9d2715d954b
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-01-28 20:13:51 -0500

    base linear regression model + collaboration form

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -1,19 +1,40 @@
 import numpy as np
 
-
 class LinearRegression:
 
+    """
+    Defining parameters
+    """
     w: np.ndarray
     b: float
 
     def __init__(self):
-        raise NotImplementedError()
+        self.w = None
+        self.b = 0
 
-    def fit(self, X, y):
-        raise NotImplementedError()
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fitting the model on X and y.
 
-    def predict(self, X):
-        raise NotImplementedError()
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The output data.
+        """
+        self.w = np.linalg.solve(X.T.dot(X), X.T.dot(y))
+        self.b = np.mean(y) - np.mean(X.dot(self.w))
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
+        return X.dot(self.w) + self.b
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -21,9 +42,14 @@ class GradientDescentLinearRegression(LinearRegression):
     A linear regression model that uses gradient descent to fit the model.
     """
 
-    def fit(
-        self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
-    ) -> None:
+    def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000) -> None:
+        """
+        Fitting the model on X and y.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The output data.
+        """
         raise NotImplementedError()
 
     def predict(self, X: np.ndarray) -> np.ndarray:
