f304ba6e964da5bbc804b30b243baf8e729d4714
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-02-07 05:02:54 -0500

    forward fix

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -34,7 +34,7 @@ class MLP(nn.Module):
         self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(64, num_classes)
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the network.
 
