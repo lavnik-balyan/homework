2c6cb4084ce98f4002649ace0c1182e913115ffd
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-02-07 05:17:42 -0500

    wider

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -30,11 +30,11 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, 128)
+        self.fc2 = nn.Linear(hidden_size, 256)
         self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(128, 32)
+        self.fc3 = nn.Linear(256, 64)
         self.relu3 = nn.ReLU()
-        self.fc4 = nn.Linear(32, num_classes)
+        self.fc4 = nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
