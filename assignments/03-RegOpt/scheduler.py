e6ab670a92fa94db4f3c21ba89f8698d56d44e88
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-02-19 23:21:24 -0500

    1000, 0.6

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -9,7 +9,7 @@ class CustomLRScheduler(_LRScheduler):
 
     """
 
-    def __init__(self, optimizer, step_size=1000, gamma=0.5, last_epoch=-1):
+    def __init__(self, optimizer, step_size=1000, gamma=0.6, last_epoch=-1):
         """
         Create a new scheduler.
 
