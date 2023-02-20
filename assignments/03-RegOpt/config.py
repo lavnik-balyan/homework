7be6015c8b84a340a9f5330a35b918fd6fe18cfd
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-02-19 20:49:46 -0500

    base impl with 6 epochs

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -13,7 +13,7 @@ from torchvision.transforms import (
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 8
+    num_epochs = 6
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
@@ -34,5 +34,6 @@ class CONFIG:
         [
             ToTensor(),
             Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+            RandomHorizontalFlip(),
         ]
     )
