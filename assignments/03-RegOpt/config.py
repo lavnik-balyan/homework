d64af4cecc461a7c316e21a4edd7409ed7c4621b
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-02-21 20:26:34 -0500

    15 epochs test

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -12,7 +12,7 @@ from torchvision.transforms import (
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 10
+    num_epochs = 15
     initial_learning_rate = 0.002
     initial_weight_decay = 0
 
