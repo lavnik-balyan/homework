425a40623cc4e8171437a7ab79fefb6246055ef3
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-03-13 16:08:27 -0400

    less conv layers

--- assignments/04-Convs/model-badtime.py
+++ assignments/04-Convs/model-badtime.py
@@ -20,11 +20,12 @@ class Model(torch.nn.Module):
         """
 
         super().__init__()
-        self.conv1 = nn.Conv2d(num_channels, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(128 * 8 * 8, 512)
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
         self.fc2 = nn.Linear(512, 64)
         self.fc3 = nn.Linear(64, num_classes)
 
@@ -36,8 +37,9 @@ class Model(torch.nn.Module):
 
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
 
-        x = x.view(-1, 128 * 8 * 8)
+        x = x.view(-1, 128 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
--- assignments/04-Convs/model-test.py
+++ assignments/04-Convs/model-test.py
@@ -19,9 +19,7 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
-        print(x.shape)
         x = x.view(-1, 128 * 8 * 8)
-        print(x.shape)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
 
