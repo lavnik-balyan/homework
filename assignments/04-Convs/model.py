375df9e653d40c0c587980376420fd921c2d8bab
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-03-14 19:09:02 -0400

    reconfig

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -18,26 +18,11 @@ class Model(torch.nn.Module):
         """
 
         super().__init__()
-<<<<<<< HEAD
-        self.conv1 = nn.Conv2d(num_channels, 8, 3, padding=1)
-        self.conv2 = nn.Conv2d(8, 16, 3, padding=1)
-        self.conv3 = nn.Conv2d(16, 32, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.fc1 = nn.Linear(32 * 4 * 4, 64)
-        self.fc2 = nn.Linear(64, num_classes)
-=======
         self.conv1 = nn.Conv2d(num_channels, 16, 3, padding=1)
         self.conv2 = nn.Conv2d(16, 16, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
-<<<<<<< HEAD
-        self.fc1 = nn.Linear(32 * 8 * 8, num_classes)
-        # self.fc2 = nn.Linear(128, num_classes)
->>>>>>> a2f31d8e4507e25ffdffb7660ab3ce9c445d7dce
-=======
         self.fc1 = nn.Linear(16 * 8 * 8, num_classes)
->>>>>>> 9227f6f2593e34bbd50349edf01ca5771940c3fb
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -47,17 +32,6 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        x = x.view(-1, 32 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-=======
-        x = x.view(-1, 32 * 8 * 8)
-        # x = F.relu(self.fc1(x))
-=======
         x = x.view(-1, 16 * 8 * 8)
->>>>>>> 9227f6f2593e34bbd50349edf01ca5771940c3fb
         x = self.fc1(x)
->>>>>>> a2f31d8e4507e25ffdffb7660ab3ce9c445d7dce
         return x
