5eb9a4e97ead2aabe1046c0bc912626cc6335372
eeshashekhar <eesha.shekhar@gmail.com>
2023-03-14 19:00:08 -0400

    committing

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,11 +6,11 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 35
+    batch_size = 40
     num_epochs = 8
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=2e-3)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=3e-3)
 
     transforms = Compose([ToTensor()])
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class Model(torch.nn.Module):
     """
     A CNN for getting 0.55 accuracy on the CIFAR-10 dataset as quickly as possible.
@@ -19,13 +18,13 @@ class Model(torch.nn.Module):
         """
 
         super().__init__()
-        self.conv1 = nn.Conv2d(num_channels, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 8, 3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, 3, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(64 * 4 * 4, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(32 * 4 * 4, 64)
+        self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -36,7 +35,7 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
-        x = x.view(-1, 64 * 4 * 4)
+        x = x.view(-1, 32 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
