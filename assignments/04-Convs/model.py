456227ede9a4a907ef8c5f5c35223e56c4339732
eeshashekhar <eesha.shekhar@gmail.com>
2023-03-14 16:40:08 -0400

    new model

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 class Model(torch.nn.Module):
     """
     A CNN for getting 0.55 accuracy on the CIFAR-10 dataset as quickly as possible.
-
     """
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
@@ -20,25 +19,24 @@ class Model(torch.nn.Module):
         """
 
         super().__init__()
-        self.conv1 = nn.Conv2d(num_channels, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, num_classes)
+        self.fc1 = nn.Linear(64 * 4 * 4, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward method for CNN.
-
         """
 
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
-        x = x.view(-1, 128 * 4 * 4)
+        x = x.view(-1, 64 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
