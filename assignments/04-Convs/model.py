aca45654c277804e49e56a074d1d11f9263d4dc8
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-03-14 19:42:27 -0400

    rand hor flip and more convs

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -19,12 +19,11 @@ class Model(torch.nn.Module):
         """
 
         super().__init__()
-        self.conv1 = nn.Conv2d(num_channels, 8, 3, padding=1)
-        self.conv2 = nn.Conv2d(8, 16, 3, padding=1)
+        self.conv1 = nn.Conv2d(num_channels, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(16 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(16 * 8 * 8, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -35,6 +34,5 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
 
         x = x.view(-1, 16 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.fc1(x)
         return x
