a5a9bd96fc60c66e8bf5c66ddacf4fd04a334335
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-03-18 15:18:47 -0400

    larger

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -20,8 +20,8 @@ class Model(torch.nn.Module):
 
         super().__init__()
         self.conv1 = nn.Conv2d(num_channels, 16, 3)
-        self.pool = nn.MaxPool2d(3, 3)
-        self.fc1 = nn.Linear(16 * 10 * 10, num_classes)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 15 * 15, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -29,6 +29,6 @@ class Model(torch.nn.Module):
         """
 
         x = self.pool(F.relu(self.conv1(x)))
-        x = x.view(-1, 16 * 10 * 10)
+        x = x.view(-1, 16 * 15 * 15)
         x = self.fc1(x)
         return x
