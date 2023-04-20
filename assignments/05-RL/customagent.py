a815130b1ea8dcb0d19bf3901828a95c19d4368a
Lavnik Balyan <balyan.lavnik@gmail.com>
2023-04-20 15:50:11 -0400

    heuristic solution gpt4

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -20,7 +20,23 @@ class Agent:
         Takes an observation and returns an action.
         """
 
-        return self.action_space.sample()
+        x, y, vx, vy, angle, angular_velocity, contact_left, contact_right = observation
+
+        # Heuristic for landing the lander
+        if abs(angle) > 0.1:
+            # If the angle is too steep, counteract the rotation
+            action = 0 if angle < 0 else 2
+        elif abs(angular_velocity) > 0.1:
+            # If the angular velocity is too high, counteract it
+            action = 0 if angular_velocity < 0 else 2
+        elif abs(vy) > 0.2 or abs(vx) > 0.1:
+            # If the lander is descending too fast or moving horizontally too fast, fire the main engine
+            action = 3
+        else:
+            # Otherwise, do nothing
+            action = 1
+
+        return action
 
     def learn(
         self,
